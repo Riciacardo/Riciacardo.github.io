401dde0091e6adbe6c2ebc59419e97c49d2b4881
Riciacardo <164555187+Riciacardo@users.noreply.github.com>
2024-03-24 10:20:15 +0100

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,4 @@
+document.getElementById("discord-btn").addEventListener("click", function() {
+  // Collegamento al tuo DM Discord
+  window.open("https://discord.com/users/1073945548790444144");
+});
